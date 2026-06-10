4a7d73e1193322b020c00365d68c8a5fea3fbb6c
Maksym Gryniuk <52629630+Maxisoft5@users.noreply.github.com>
2026-06-10 23:46:54 +0300

    upd

--- WorkoutLogg/WorkoutLogger.WebApi/Program.cs
+++ WorkoutLogg/WorkoutLogger.WebApi/Program.cs
@@ -1,11 +1,33 @@
 using Microsoft.EntityFrameworkCore;
 using Modules.Common.Infrastructure.Extensions;
 using Modules.Users.Infrastructure.Database;
+using Serilog;
+using Serilog.Sinks.OpenSearch;
 using System.Text.Json.Serialization;
 using WorkoutLogger.WebApi.Extensions;
 using WorkoutLogger.WebApi.Grpc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((ctx, _, config) =>
+{
+    var openSearchUrl = ctx.Configuration["OpenSearch:Url"] ?? "http://opensearch:9200";
+
+    config
+        .ReadFrom.Configuration(ctx.Configuration)
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .Enrich.WithEnvironmentName()
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .WriteTo.OpenSearch(new OpenSearchSinkOptions(new Uri(openSearchUrl))
+        {
+            AutoRegisterTemplate = true,
+            IndexFormat = "workoutlogger-logs-{0:yyyy.MM.dd}",
+            NumberOfShards = 1,
+            NumberOfReplicas = 0,
+            EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog
+        });
+});
 
 builder.AddServiceDefaults();
 
